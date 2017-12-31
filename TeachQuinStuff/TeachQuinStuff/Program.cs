e3e7fa08a749534eff1c25c23d86ab1bad5d1f21
patrickh94 <patrick@connectuity.com>
2017-12-31 04:53:14 -0500

    stuff looking more complex, hopefully quin can decipher what this does

--- TeachQuinStuff/TeachQuinStuff/Program.cs
+++ TeachQuinStuff/TeachQuinStuff/Program.cs
@@ -7,8 +7,37 @@ namespace TeachQuinStuff
     {
         public static void Main(string[] args)
         {
-            Quin quin = new Quin();
-            quin.Talk();
+            Quin mainQuin = new Quin();
+            mainQuin.Talk();
+            BuyArt(mainQuin);
+        }
+
+        private static void BuyArt(Quin someKindOfQuin)
+        {
+            bool quinHasAgreedToMakeArt = false;
+            int moneyOfferedToQuin = 1500;
+
+            while(!quinHasAgreedToMakeArt)
+            {
+                Console.WriteLine("What art would you like from Quin?");
+                string artIWantFromQuin = Console.ReadLine();
+                if (someKindOfQuin.AskForArt(artIWantFromQuin, moneyOfferedToQuin))
+                {
+                    Console.WriteLine("Sweet, Quin said yes!");
+                    CustomerIsSatisfied();
+                    quinHasAgreedToMakeArt = true;
+                }
+                else
+                {
+                    moneyOfferedToQuin += 250;
+                    Console.WriteLine("Oh, Quin said no.");
+                }
+            }
+        }
+
+        private static void CustomerIsSatisfied()
+        {
+            Console.WriteLine("Customer: Sweet, I'm a happy customer.");
         }
     }
 }
